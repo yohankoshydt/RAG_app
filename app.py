d78e7521fd4a1422e3b204ec35fcd774ce72b861
yohankoshydt <147714807+yohankoshydt@users.noreply.github.com>
2024-12-10 18:25:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from RAG import QueryHandler
 # Load data from Excel
 file_path = r'Sales_Sol_Sheet.csv'
 df = read_file(file_path)
+industries = df['Industry'].unique().tolist()
+industries.append('Other')
 
 # Initialize QueryHandler
 query_handler = QueryHandler()
@@ -38,7 +40,7 @@ if 'context' not in st.session_state:
         'case_studies': [],
         'dashboards': [],
         'Rec_summary': 'No description available',
-        'Industry': 'No industry information available',
+        'Industry': None,
         'clients': 'Emerson, Barry Wehmiller, Elkay Silicones, Afnitas, Alleima, Trimble'
     }
 
@@ -56,13 +58,21 @@ def update_context(context, presets, query_handler, df):
         if isinstance(summary_data, dict):
             # Update context with Industry and Description
             st.session_state.context.update({
-                'Rec_summary': summary_data.get('Description', 'No description available'),
-                'Industry': summary_data.get('Industry', 'No industry information available')
+                'Rec_summary': summary_data.get('Description', 'No description available')
+                
             })
 
             # Filter DataFrame by Industry and Function
-            filtered_df = df[(df['Industry'] == context['Industry']) &
-                             (df['Function'] == presets['Recipient_Function'])]
+            print(f'{context['Industry'], {presets['Recipient_Function']}}')
+            filtered_df = df[
+                            (df['Industry'] == context['Industry']) &
+                            (
+                                (df['Function'] == presets['Recipient_Function']) |
+                                (df['Function'] == 'All')
+                            )
+                        ]
+            
+            print(filtered_df.head())
 
             # Create case_studies list
             case_studies = []
@@ -109,6 +119,12 @@ with st.form("details_form"):
     st.session_state.presets['Recipient_Company'] = st.text_input("Recipient Company", st.session_state.presets['Recipient_Company'])
     st.session_state.presets['Website'] = st.text_input("Website", st.session_state.presets['Website'])
     
+    st.write("Recipient Industry")
+    st.session_state.context['Industry'] = st.selectbox(
+        "Select Industry",
+        industries,
+        key="industry_dropdown"
+    )
     st.header("Enter Sender Details")
     st.session_state.context['Sender_Company'] = st.text_input("Sender Company", st.session_state.context['Sender_Company'])
     st.session_state.context['Sender_Name'] = st.text_input("Sender Name", st.session_state.context['Sender_Name'])
